bbc0daf0808a30670152991924099e8ae0a82ab1
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-11-22 11:51:53 -0300

    First commit. Users can be added, removed and edited via API requisitions.

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,33 @@
+package com.example.demo.model;
+
+public class User {
+    private String ID;
+    private String name;
+    private int age;
+
+    public User(String ID, String name, int age){
+        this.ID = ID;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
