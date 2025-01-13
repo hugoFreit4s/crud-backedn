0dba1aa2f88ea962c93ce2fd8a8ff8f08b892bec
Hugo de Freitas <hugo.encaminhados@gmail.com>
2025-01-13 13:59:39 -0300

    changes at User model

--- src/main/java/com/example/demo/dto/UserResponseDTO.java
+++ src/main/java/com/example/demo/dto/UserResponseDTO.java
@@ -1,5 +1,6 @@
 package com.example.demo.dto;
 
+import lombok.Data;
 import org.springframework.cglib.core.Local;
 
 import java.time.Duration;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
+@Data
 public class UserResponseDTO {
     private int ID;
     private String name;
@@ -15,61 +17,15 @@ public class UserResponseDTO {
     private String phone;
     private long age;
     private List<UserCarsResponseDTO> cars;
+    private LocalDate birthDate;
 
-    public UserResponseDTO(int ID, String name, long age, String birthDate, String gender, String phone, List<UserCarsResponseDTO> cars) {
+    public UserResponseDTO(int ID, String name, long age, LocalDate birthDate, String gender, String phone, List<UserCarsResponseDTO> cars) {
         this.ID = ID;
         this.name = name;
         this.gender = gender;
         this.phone = phone;
         this.cars = cars;
         this.age = age;
-    }
-
-    public void setCars(List<UserCarsResponseDTO> cars) {
-        this.cars = cars;
-    }
-
-    public int getID() {
-        return ID;
-    }
-
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public long getAge() {
-        return age;
-    }
-
-    public void setAge(long age) {
-        this.age = age;
-    }
-
-    public List<UserCarsResponseDTO> getCars() {
-        return cars;
+        this.birthDate = birthDate;
     }
 }
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -2,13 +2,13 @@ package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
 
-import java.time.Duration;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 
+@Data
 @Entity
 public class User {
     @Id
@@ -17,95 +17,20 @@ public class User {
     private String name;
     private long age;
     private String gender;
-    private String birthDate;
+    private LocalDate birthDate;
     private String phone;
     @OneToMany(mappedBy = "owner")
     @JsonManagedReference
     private List<Car> cars;
 
-    public User(String name, String gender, String birthDate, String phone) {
+    public User(String name, String gender, LocalDate birthDate, String phone) {
         this.name = name;
         this.gender = gender;
         this.birthDate = birthDate;
         this.phone = phone;
-        this.age = calcAge(birthDate);
+        this.age = 0; //TODO: age calc
     }
 
     public User() {
-    }
-
-    public long calcAge(String birthDate) {
-        String[] stringDate = birthDate.split("-");
-        int year = Integer.parseInt(stringDate[0]);
-        int month = Integer.parseInt(stringDate[1]);
-        int day = Integer.parseInt(stringDate[2]);
-        LocalDate formattedBirthDate = LocalDate.of(year, month, day);
-        LocalDateTime today = LocalDateTime.now();
-        LocalDateTime formattedBirthDateTime = LocalDateTime.of(formattedBirthDate, LocalTime.now());
-        long daysDiff = Duration.between(formattedBirthDateTime, today).toDays();
-        return daysDiff / 365;
-    }
-
-    @PostLoad
-    @PostPersist
-    @PostUpdate
-    private void calcAgeAfterLoad() {
-        this.age = calcAge(this.birthDate);
-    }
-
-    public long getAge() {
-        return age;
-    }
-
-    public void setAge(long age) {
-        this.age = age;
-    }
-
-    public int getID() {
-        return ID;
-    }
-
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public List<Car> getCars() {
-        return cars;
-    }
-
-    public void setCars(List<Car> cars) {
-        this.cars = cars;
     }
 }
